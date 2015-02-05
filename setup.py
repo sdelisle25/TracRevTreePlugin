ff85c9ab93cecafc4adda9c996e849267f80579f
Emmanuel Bouaziz <ebouaziz@neotion.com>
2015-02-05 19:12:57 +0100

    Stop using old DB API as it has been removed

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup, find_packages
 
 PACKAGE = 'TracRevtreePlugin'
-VERSION = '2.0.3'
+VERSION = '2.0.4'
 
 setup(
     name=PACKAGE,
@@ -26,7 +26,7 @@ setup(
     license='BSD',
     url='http://trac-hacks.org/wiki/RevtreePlugin',
     keywords="trac revision svg graphical tree browser visual",
-    install_requires=['Trac>=0.12dev', 'Trac<1.2'],
+    install_requires=['Trac>=1.1.3', 'Trac<1.2'],
     packages=find_packages(exclude=['ez_setup', '*.tests*', '*.enhancers.*']),
     package_data={
         'revtree': [
